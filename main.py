9c8b7e6443b1f84e8f948da2429606606c710465
Benjamin Poulin <124279947+bpoulin7@users.noreply.github.com>
2023-03-15 01:54:37 +0000

    Added option to move around or exit game, and applied formatting.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-# global variables
+#-----------------------------------------------------------------------------
+# Created by Ben P
+# Date: 14/3/2023
+# version 001
+#-----------------------------------------------------------------------------
+"""A simple map movement game set in Ancient Mesopotamia."""
+#-----------------------------------------------------------------------------
 
-row = 4
-col = 4
-
+# Global Variables ###########################################################
+row = 4  # east-west coordinate reference on map
+col = 4  # north-south coordinate reference on map
+# simplified map of 15th century BCE Mesopotamia
+# starting location is Ur (4,4)
 map = [
     ["Karkemish","Washukanni","Nineveh","Erbil","mountains"],
     ["Ugarit","Emar","Ashur","Arrapkha","mountains"],
@@ -10,50 +18,60 @@ map = [
     ["Tyre","desert","desert","Babylon","Susa"],
     ["Jericho","desert","desert","Uruk","Ur"]
 ]
-
-#sub-menu (which direction)
-
 valid_directions = ["north", "n", "south", "s", "east", "e", "west", "w"]
 
+# Functions ##################################################################
+
+
 def movement():
+    """Allows movement around the map. Requires cardinal direction input,
+    prevents movement outside map boundary, and accounts for invalid input.
+    """
     global row, col
     direction_input = input("Which direction? ")
     if direction_input in valid_directions:
-        if direction_input.lower() == "north" or direction_input.lower() == "n":
+        if (direction_input.lower() == "north" or
+            direction_input.lower() == "n"):
             if row == 0:
                 print("You have reached the edge of civilization.")
             elif row > 0:
                 row = row - 1
-        elif direction_input.lower() == "south" or direction_input.lower() == "s":
+        elif (direction_input.lower() == "south" or
+              direction_input.lower() == "s"):
             if row == 4:
                 print("You have reached the edge of civilization.")
             elif row <4:
                 row = row + 1
-        elif direction_input.lower() == "east" or direction_input.lower() == "e":
+        elif (direction_input.lower() == "east" or
+              direction_input.lower() == "e"):
             if col == 4:
                 print("You have reached the edge of civilization.")
             elif col < 4:
                 col = col + 1
-        elif direction_input.lower() == "west" or direction_input.lower() == "w":
+        elif (direction_input.lower() == "west" or
+              direction_input.lower() == "w"):
             if col == 0:
                 print("You have reached the edge of civilization.")
             elif col > 0:
                 col = col - 1
+    # accounting for invalid input
     else:
         print("Invalid direction!")
 
-# main
+
+# Main #######################################################################
 
 while True:
+  # prints description of current location and brings up option menu
   current_location = map[row][col]
   if current_location == "desert":
-      print("There are only sand dunes around, and no civilization for miles.")
+      print("There are only sand dunes around, and no civilization for miles")
       print("It is not wise to stay here long.")
   elif current_location == "mountains":
     print("Vast mountains stretch as far as the eye can see.")
     print("It is not wise to stay here long.")
   elif current_location == "Karkemish":
-    print("Welcome to Karkemish, a legendary and enigmatic city on the Euphrates River.")
+    print("Welcome to Karkemish, a legendary city on the Euphrates River.")
   elif current_location == "Washukanni":
     print("Welcome to Washukanni, the great capital of the Mittani Empire.")
   elif current_location == "Nineveh":
@@ -61,33 +79,43 @@ while True:
   elif current_location == "Erbil":
     print("Welcome to Erbil, a town on the edge of the civilized world.")
   elif current_location == "Ugarit":
-    print("Welcome to Ugarit, a prominent trading hub city on the Mediterranean coast.")
+    print("Welcome to Ugarit, a trading hub city on the Mediterranean coast.")
   elif current_location == "Emar":
-    print("Welcome to Emar, a small but important trade hub on the Euphrates River.")
+    print("Welcome to Emar, a small important trade hub on the Euphrates.")
   elif current_location == "Ashur":
-    print("Welcome to Ashur, the capital of Assyria, now under control of the Mittani.")
+    print("Welcome to Ashur, capital of Assyria, now under Mittani control.")
   elif current_location == "Arrapkha":
-    print("Welcome to Arrapkha, a secluded city on the eastern edge of the known world.")
+    print("Welcome to Arrapkha, a secluded city on the edge of civilization.")
   elif current_location == "Sidon":
-    print("Welcome to Sidon, an ancient fishing town under Egyptian influence.")
+    print("Welcome to Sidon, an ancient fishing town under Egyptian control.")
   elif current_location == "Mari":
-    print("Welcome to Mari, once the center of an empire, now largely abandoned.")
+    print("Welcome to Mari, once a thriving city, now mostly abandoned.")
   elif current_location == "Dur-Kurigalzu":
     print("Welcome to Dur-Kurigalzu, the capital of Kassite Babylonia.")
   elif current_location == "Sippar":
     print("Welcome to Sippar, one of the many riverine cities of Babylonia.")
   elif current_location == "Tyre":
-    print("Welcome to Tyre, one of the oldest cities on the Mediterranean coast.")
+    print("Welcome to Tyre, one of the oldest cities on the Mediterranean.")
   elif current_location == "Babylon":
-    print("Welcome to Babylon, the largest city in the world and heart of Babylonia.")
+    print("Welcome to Babylon, the heart of Babylonia and Mesopotamia.")
   elif current_location == "Susa":
     print("Welcome to Susa, a major city of the Elamite Empire.")
   elif current_location == "Jericho":
     print("Welcome to Jericho, perhaps the oldest city in the world.")
   elif current_location == "Uruk":
-    print("Welcome to Uruk, an old Sumerian city in the middle of Mesopotamia.")
+    print("Welcome to Uruk, an old Sumerian city central to Mesopotamia.")
   elif current_location == "Ur":
-    print("Welcome to Ur, a Sumerian city on the raised Persian Gulf coast.")
-  
-  print("There are no options yet, so walk!")
-  movement()
+    print("Welcome to Ur, a Sumerian city on the high Persian Gulf coast.")
+  # player action options (move or exit game)
+  input_choice = input("What would you like to do? ")
+  if (input_choice.lower() == "walk" or input_choice.lower() == "move" or
+      input_choice.lower() == "go"):
+      movement()
+  elif input_choice.lower() == "quit" or input_choice == "exit":
+      confirm_quit = input("Are you sure you want to quit? ")
+      if confirm_quit.lower() == "yes" or confirm_quit.lower() == "y":
+          print("Leaving game...")
+          quit()
+  # accounting for invalid input
+  else:
+      print("Invalid choice!")
